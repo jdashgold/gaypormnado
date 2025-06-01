a6bd05cf31b087f6f9bf162cb59bab76290ef26c
jdashgold <jdashgold@gmail.com>
2025-06-01 22:49:13 +0800

    Set Node version and configure SSR Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
+import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });
